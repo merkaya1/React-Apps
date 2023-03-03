95b9642f4aaa0995588f2202c666a5a5a7e993d3
MUSTAFA ERKAYA <TCMUERKAYA@turkcell.entp.tgc>
2023-03-03 10:54:41 +0300

    guide js finish

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Companies from './Components/Companies';
+import Guide from './Components/Guide';
 import Hero from './Components/Hero';
 
 function App() {
   return (
     <div className='App'>
       <Hero />
+      <Companies />
+      <Guide />
     </div>
   );
 }
